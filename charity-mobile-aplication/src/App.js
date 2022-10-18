22814dab0226c8e6a41963e28d5a18d634ed971c
Dakerman922 <111058750+Dakerman922@users.noreply.github.com>
2022-10-18 12:21:40 +0300

    Add files via upload

---
+++ charity-mobile-aplication/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import kotyk from './images/kotyk.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={kotyk} alt="Logo" />;
+      <h1>Все буде добре :(</h1>
+    </div>
+  );
+}
+
+export default App;
